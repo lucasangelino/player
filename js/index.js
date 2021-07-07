170497785c0c68424509d38ecc839405c969fc6d
Lucas Angelino <angelino.n.lucas@gmail.com>
2021-07-06 21:05:23 -0300

    PLY-02 adding buttons and backward and forward functionality

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const BACKWARD_SECONDS = 10;
+const FORWARD_SECONDS = 10;
+const $video = document.querySelector("#player");
+const $play = document.querySelector("#play");
+const $pause = document.querySelector("#pause");
+const $forward = document.querySelector("#forward");
+const $backward = document.querySelector("#backward");
+
+$play.addEventListener("click", handlePlay);
+$pause.addEventListener("click", handlePause);
+$backward.addEventListener("click", handleBackward);
+$forward.addEventListener("click", handleForward);
+
+function handlePlay() {
+  $video.play();
+  $play.hidden = true;
+  $pause.hidden = false;
+}
+function handlePause() {
+  $video.pause();
+  $play.hidden = false;
+  $pause.hidden = true;
+}
+
+function handleBackward() {
+  $video.currentTime = $video.currentTime - BACKWARD_SECONDS;
+}
+
+function handleForward() {
+  $video.currentTime = $video.currentTime + FORWARD_SECONDS;
+}
